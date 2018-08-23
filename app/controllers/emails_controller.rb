bfc969c23349d94ba078cfd45110debe5d1a85de
Dim <dimitri.kiavue@gmail.com>
2018-08-23 15:12:20 +0200

    Merge OK

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,14 +1,24 @@
 class EmailsController < ApplicationController
   def index
-    @email = Email.all
+    @email_all = Email.all
   end
 
   def new
   end
 
   def create
+    @email = Email.new(email_params)
+    if @email.save
+      flash["success"] = "Email created"
+    end
   end
 
   def destroy
   end
 end
+
+private
+
+  def email_params
+    params.require(:email).permit(:object, :body)
+  end
