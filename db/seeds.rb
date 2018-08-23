1702b487ed486aa4a7516ee2eaff1c6c29f157f6
nicolasculerier <nicolas.culerier@laposte.net>
2018-08-23 11:06:16 +0200

    Creation model, seeds etc...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Email.create!(object: "A Chuck Norris's quote", body: Faker::ChuckNorris.fact)
+end
